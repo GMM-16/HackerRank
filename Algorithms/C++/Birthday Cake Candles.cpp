bed75e920d7be1c633b2339d30e61a56f795d861
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-01 20:49:38 +0530

    Create Birthday Cake Candles.cpp

---
+++ Algorithms/C++/Birthday Cake Candles.cpp
@@ -0,0 +1,123 @@
+/*
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+birthdayCakeCandles has the following parameter(s):
+int candles[n]: the candle heights
+Returns
+- int: the number of candles that are tallest
+
+Input Format
+The first line contains a single integer, n, the size of candles[].
+The second line contains n space-separated integers, where each integer i describes the height of candles[i].
+
+Constraints
+1 <= n <= 10^5
+1 <= candles[i] <= 10^7
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+int birthdayCakeCandles(vector<int> candles) {
+    int tall,size,ans;
+    tall = ans = 0;
+    size = candles.size();
+    for(int i=0;i<size;i++)
+    {
+        if(candles[i]>tall)
+        {
+            tall = candles[i];
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        if(candles[i]==tall)
+        {
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string candles_count_temp;
+    getline(cin, candles_count_temp);
+
+    int candles_count = stoi(ltrim(rtrim(candles_count_temp)));
+
+    string candles_temp_temp;
+    getline(cin, candles_temp_temp);
+
+    vector<string> candles_temp = split(rtrim(candles_temp_temp));
+
+    vector<int> candles(candles_count);
+
+    for (int i = 0; i < candles_count; i++) {
+        int candles_item = stoi(candles_temp[i]);
+
+        candles[i] = candles_item;
+    }
+
+    int result = birthdayCakeCandles(candles);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
