d46fc9d6d1a758527f7be3d164cf0aa9655df48c
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-03 16:56:48 +0530

    Create Divisible Sum Pairs.cpp

---
+++ Algorithms/C++/Divisible Sum Pairs.cpp
@@ -0,0 +1,128 @@
+/*
+Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i < j and ar[i] + ar[j] is divisible by k.
+
+Function Description
+Complete the divisibleSumPairs function in the editor below.
+divisibleSumPairs has the following parameter(s):
+int n: the length of array ar
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+The first line contains 2 space-separated integers, n and k.
+The second line contains n space-separated integers, each a value of arr[i].
+
+Constraints
+2 <= n <= 100
+1 <= k <= 100
+1 <= arr[i] <= 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+    int ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            int sum = 0;
+            sum = ar[i]+ar[j];
+            if(sum%k==0)
+            {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split(rtrim(ar_temp_temp));
+
+    vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = divisibleSumPairs(n, k, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
