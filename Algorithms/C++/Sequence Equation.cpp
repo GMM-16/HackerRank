7e63407efedda2ce34d254377f3b2f813ef02671
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-07 20:07:02 +0530

    Create Sequence Equation.cpp

---
+++ Algorithms/C++/Sequence Equation.cpp
@@ -0,0 +1,126 @@
+/*
+Given a sequence of n integers, p(1),p(2).....p{n) where each element is distinct and satisfies 1 <= p(x) <= n. For each x where 1 <= x <= n, that is x increments from 1 to n, find any integer y such that p(p(y)) ≡ x and keep a history of the values of y in a return array.
+
+Function Description
+Complete the permutationEquation function in the editor below.
+permutationEquation has the following parameter(s):
+int p[n]: an array of integers
+Returns
+- int[n]: the values of y for all x in the arithmetic sequence 1 to n.
+
+Input Format
+The first line contains an integer n, the number of elements in the sequence.
+The second line contains n space-separated integers p[i] where 1 <= i <= n.
+
+Constraints
+1 <= n <= 50
+1 <= p[i] <= 50 where 1 <= i <= n.
+Each element in the sequence is distinct.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'permutationEquation' function below.
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+vector<int> permutationEquation(vector<int> p) {
+    vector<int> ans;
+    int size = p.size();
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {   
+            if(p[p[j]-1]==i+1)
+            {
+                ans.push_back(j+1);
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp_temp;
+    getline(cin, p_temp_temp);
+
+    vector<string> p_temp = split(rtrim(p_temp_temp));
+
+    vector<int> p(n);
+
+    for (int i = 0; i < n; i++) {
+        int p_item = stoi(p_temp[i]);
+
+        p[i] = p_item;
+    }
+
+    vector<int> result = permutationEquation(p);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
