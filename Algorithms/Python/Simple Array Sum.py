6c03a189fbf38e18f29d535a393d957f7ae12c3c
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-09 18:35:14 +0530

    Create Simple Array Sum.py

--- Algorithms/Python/Simple Array Sum.py
+++ Algorithms/Python/Simple Array Sum.py
@@ -2,7 +2,6 @@
 Given an array of integers, find the sum of its elements.
 
 Function Description
-
 Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
 simpleArraySum has the following parameter(s):
 ar: an array of integers
