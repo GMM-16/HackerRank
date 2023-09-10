410da3634b46ad5e637b0dc7338a21dd49b67372
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-10 18:32:06 +0530

    Create Diagonal Difference.py

--- Algorithms/Python/Diagonal Difference.py
+++ Algorithms/Python/Diagonal Difference.py
@@ -29,7 +29,6 @@ import sys
 
 #
 # Complete the 'diagonalDifference' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 #
