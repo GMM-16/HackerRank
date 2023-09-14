a94b33434b11fa4892ba39f23811a2dc48a43d04
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-14 22:47:08 +0530

    Create Cat and a Mouse.py

---
+++ Algorithms/Python/Cat and a Mouse.py
@@ -0,0 +1,63 @@
+'''
+Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+You are given q queries in the form of x, y, and z representing the respective positions for cats A and B, and for mouse C. Complete the function catAndMouse to return the appropriate answer to each query, which will be printed on a new line.
+If cat A catches the mouse first, print Cat A.
+If cat B catches the mouse first, print Cat B.
+If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+
+Function Description
+Complete the catAndMouse function in the editor below.
+catAndMouse has the following parameter(s):
+int x: Cat A's position
+int y: Cat B's position
+int z: Mouse C's position
+Returns
+- string: Either 'Cat A', 'Cat B', or 'Mouse C'
+
+Input Format
+The first line contains a single integer, q, denoting the number of queries.
+Each of the q subsequent lines contains three space-separated integers describing the respective values of x (cat A's location), y (cat B's location), and z (mouse C's location).
+
+Constraints
+1 <= q <= 100
+1 <= x,y,z <= 100
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    from_A = abs(z - x)
+    from_B = abs(z - y)
+    if from_A == from_B:
+        return "Mouse C"
+    elif from_A > from_B:
+        return "Cat B"
+    elif from_A < from_B:
+        return "Cat A"    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
