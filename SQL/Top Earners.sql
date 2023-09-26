899bfef9d6870f9d263c680ae8a636654156d949
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-09-26 21:25:57 +0530

    Create Top Earners.sql

---
+++ SQL/Top Earners.sql
@@ -0,0 +1,16 @@
+/*
+We define an employee's total earnings to be their monthly salary*months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
+
+TABLE EMPLOYEE
+
+Column        |    Type
+              |
+EMPLOYEE_ID   |    INTEGER
+NAME          |    STRING
+MONTHS        |    INTEGER
+SALARY        |    INTEGER
+
+where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
+*/
+
+SELECT MAX(SALARY*MONTHS),COUNT(SALARY*MONTHS) FROM EMPLOYEE GROUP BY SALARY*MONTHS DESC LIMIT 1
