decc4179de0692f4be0648f1a4ce1868feea7a2e
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-10-11 19:27:05 +0530

    Create Lisa's Workbook.cpp

---
+++ Algorithms/C++/Lisa's Workbook.cpp
@@ -0,0 +1,136 @@
+/*
+Lisa just got a new math workbook. A workbook contains exercise problems, grouped into chapters. Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located. The format of Lisa's book is as follows:
+There are  chapters in Lisa's workbook, numbered from 1 to n.
+The ith chapter has arr[i] problems, numbered from 1 to arr[i].
+Each page can hold up to k problems. Only a chapter's last page of exercises may contain fewer than k problems.
+Each new chapter starts on a new page, so a page will never contain problems from more than one chapter.
+The page number indexing starts at 1.
+Given the details for Lisa's workbook, can you count its number of special problems?
+
+Function Description
+Complete the workbook function in the editor below.
+workbook has the following parameter(s):
+int n: the number of chapters
+int k: the maximum number of problems per page
+int arr[n]: the number of problems in each chapter
+Returns
+- int: the number of special problems in the workbook
+
+Input Format
+The first line contains two integers n and k, the number of chapters and the maximum number of problems per page.
+The second line contains n space-separated integers arr[i] where arr[i] denotes the number of problems in the ith chapter.
+
+Constraints
+1 <= n,k,arr[i] <= 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'workbook' function below.
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY arr
+ */
+
+int workbook(int n, int k, vector<int> arr) {
+    int special = 0;
+    int page = 1;
+    for (int chapter = 0; chapter < n; chapter++) 
+    {
+        int prob = arr[chapter];
+        for (int problem = 1; problem <= prob; problem++) 
+        {
+            if (problem == page) 
+            {
+                special = special + 1;
+            }
+            if (problem % k == 0 || problem == prob) 
+            {
+                page = page + 1;
+            }
+        }
+    }
+    return special;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = workbook(n, k, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
