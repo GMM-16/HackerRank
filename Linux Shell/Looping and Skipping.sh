03b71c5f07287aed11b84cad99191594af523f79
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-11-11 23:08:55 +0530

    Create Looping and Skipping.sh

---
+++ Linux Shell/Looping and Skipping.sh
@@ -0,0 +1,25 @@
+: '
+Your task is to use for loops to display only odd natural numbers from 1 to 99.
+
+Input Format
+There is no input.
+
+Constraints
+-
+
+Output Format
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+'
+
+for((i=1;i<=99;i+=2))
+    do
+        echo $i
+    done
