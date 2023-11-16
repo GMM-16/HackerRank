95ac87087fe3e1e7123230f33238c9c708311822
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-11-16 18:53:32 +0530

    Create Cut #3.sh

--- Linux Shell/Cut #3.sh
+++ Linux Shell/Cut #3.sh
@@ -12,7 +12,7 @@ The output should contain N lines.
 Each line should contain the range of characters starting at the 2nd position of a string and ending at the 7th position (both positions included).
 '
 
-while read line; 
+while read line
 do 
   echo $line | cut -b 2-7
 done
