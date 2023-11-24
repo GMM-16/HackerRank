b15759838842ef8e698434b0eea4bc714f53873d
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-11-24 18:25:56 +0530

    Create 'Sed' command #2.sh

---
+++ Linux Shell/'Sed' command #2.sh
@@ -0,0 +1,18 @@
+: '
+Objective
+In this challenge, we will practice using the sed command to parse and transform text.
+
+Resources
+Sed is a popular tool that enables quick parsing and transformation of text.
+
+Task
+For each line in a given input file, transform all the occurrences of the word 'thy' with 'your'. The search should be case insensitive, i.e. 'thy', 'Thy', 'tHy' etc. should be transformed to 'your'.
+
+Input Format
+A text file will be piped into your command via STDIN.
+
+Output Format
+Transform and display the text as required in the task.
+'
+
+sed 's/\bthy\b/your/ig'
