4dcd52ed0b29369ae4757b8331c28f71762ef288
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-11-26 16:44:11 +0530

    Create 'Sed' command #3.sh

--- Linux Shell/'Sed' command #3.sh
+++ Linux Shell/'Sed' command #3.sh
@@ -2,13 +2,13 @@
 Sed is a popular utility which enables quick parsing and transformation of text.
 
 Task
-Given an input file, in each line, highlight all the occurrences of 'thy' by wrapping them up in brace brackets. The search should be case-insensitive.
+Given an input file, in each line, highlight all the occurrences of "thy" by wrapping them up in brace brackets. The search should be case-insensitive.
 
 Input Format
 A text file will be piped to your command via STDIN.
 
 Output Format
-Highlight all occurrences of 'thy' as shown in the example below.
+Highlight all occurrences of "thy" as shown in the example below.
 '
 
 sed 's/thy/{&}/ig'
