f40a57cc9bfedadf0a75375201ce8aac9162317c
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-11-26 16:52:35 +0530

    Create Count the number of elements in an Array.sh

--- Linux Shell/Count the number of elements in an Array.sh
+++ Linux Shell/Count the number of elements in an Array.sh
@@ -2,7 +2,7 @@
 Given a list of countries, each on a new line, your task is to read them into an array and then display the count of elements in that array.
 
 Recommended References
-Here's a great tutorial tutorial with useful examples related to arrays in Bash.
+Here is a great tutorial tutorial with useful examples related to arrays in Bash.
 
 Input Format
 A list of country names. The only characters present in the country names will be upper or lower case characters and hyphens.
