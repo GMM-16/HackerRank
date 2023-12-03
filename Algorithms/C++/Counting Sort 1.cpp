07f2ae8e2127413e213b25f1725fbccbd9382b89
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2023-12-03 22:02:57 +0530

    Create Counting Sort 1.cpp

---
+++ Algorithms/C++/Counting Sort 1.cpp
@@ -0,0 +1,134 @@
+/*
+Comparison Sorting
+Quicksort usually has a running time of n*log(n), but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat n*log(n) (worst-case) running time, since n*log(n) represents the minimum number of comparisons needed to know where to place each element. For more details, you can see these notes (PDF).
+
+Alternative Sorting
+Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
+Note
+For this exercise, always return a frequency array with 100 elements. The example above shows only the first 4 elements, the remainder being zeros.
+
+Challenge
+Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+Function Description
+Complete the countingSort function in the editor below.
+countingSort has the following parameter(s):
+arr[n]: an array of integers
+Returns
+- int[100]: a frequency array
+
+Input Format
+The first line contains an integer n, the number of items in arr.
+Each of the next n lines contains an integer arr[i] where 0 <= i <= n.
+
+Constraints
+100 <= n <= 10^6
+0 <= arr[i] < 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countingSort' function below.
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+vector<int> countingSort(vector<int> arr) 
+{
+    vector<int> ans;
+    vector<int> result(100, 0);
+    for(int i = 0; i < arr.size();i++)
+    {
+        result[arr[i]] += 1;
+    }
+    for(int i = 0; i < result.size();i++)
+    {
+        ans.insert(ans.end(), result[i], i);
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = countingSort(arr);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
