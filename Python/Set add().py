9b41e8d67ebd80ca89c4dc522d776c67efc5da85
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2024-02-03 18:23:00 +0530

    Create Set add().py

--- Python/Set add().py
+++ Python/Set add().py
@@ -1,1 +1,27 @@
+'''
+If we want to add a single element to an existing set, we can use the .add() operation.
+It adds the element to the set and returns 'None'.
 
+Task
+Apply your knowledge of the .add() operation to help your friend Rupal.
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+Find the total number of distinct country stamps.
+
+Input Format
+The first line contains an integer N, the total number of country stamps.
+The next N lines contains the name of the country where the stamp is from.
+
+Constraints
+0 < N <1000
+
+Output Format
+Output the total number of distinct country stamps on a single line.
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set('')
+for i in range(n):
+    s.add(str(input()))
+result = len(s)
+print(result)
