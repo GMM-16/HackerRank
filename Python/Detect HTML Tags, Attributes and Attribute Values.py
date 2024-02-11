9d0af4bf1f0c20793151b49af0f3c2fdb6fe491a
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2024-02-11 23:25:24 +0530

    Create Detect HTML Tags, Attributes and Attribute Values.py

---
+++ Python/Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,43 @@
+'''
+You are given an HTML code snippet of N lines.
+Your task is to detect and print all the HTML tags, attributes and attribute values.
+Print the detected items in the following format:
+
+Tag1
+Tag2
+-> Attribute2[0] > Attribute_value2[0]
+-> Attribute2[1] > Attribute_value2[1]
+-> Attribute2[2] > Attribute_value2[2]
+Tag3
+-> Attribute3[0] > Attribute_value3[0]
+
+The -> symbol indicates that the tag contains an attribute. It is immediately followed by the name of the attribute and the attribute value.
+The > symbol acts as a separator of attributes and attribute values.
+If an HTML tag has no attribute then simply print the name of the tag.
+Note: Do not detect any HTML tag, attribute or attribute value inside the HTML comment tags (<!-- Comments -->). Comments can be multiline.
+All attributes have an attribute value.
+
+Input Format
+The first line contains an integer N, the number of lines in the HTML code snippet.
+The next N lines contain HTML code.
+
+Constraints
+0 < N < 100
+
+Output Format
+Print the HTML tags, attributes and attribute values in order of their occurrence from top to bottom in the snippet.
+Format your answers as explained in the problem statement.
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+import sys
+N = int(input())
+S = sys.stdin.read()
+S = re.sub(r"<!--[\S\s]+?-->","",S)
+tags = re.findall(r"<[^/][\S\s]*?>",S)
+for tag in tags:
+    attrs = re.findall(r'([\w-]+)="(.+?)"',tag)
+    print(*re.findall("(?<=<)\w+",tag))
+    for attr in attrs :
+        print(f"-> {attr[0]} > {attr[1]}")
