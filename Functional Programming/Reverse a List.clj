901759e84c2ab7bb9ff190080c454cf0d9800547
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2024-03-02 20:48:27 -0500

    Create Reverse a List.clj

---
+++ Functional Programming/Reverse a List.clj
@@ -0,0 +1,11 @@
+comment("
+You are given a list of N elements. Reverse the list without using the reverse function. The input and output portions will be handled automatically. You need to write a function with the recommended method signature.
+
+Input Format
+Each element, X, of the list is displayed on a separate line.
+
+Output Format
+The output is the reverse of the input list.
+")
+
+(fn[lst] (reverse lst))
