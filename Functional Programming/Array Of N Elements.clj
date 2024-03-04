e2ed166eaf6bc9c45cb5960cf882527dba487cb1
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2024-03-04 21:59:09 +0530

    Create Array Of N Elements.clj

--- Functional Programming/Array Of N Elements.clj
+++ Functional Programming/Array Of N Elements.clj
@@ -1,1 +1,31 @@
+comment("
+Create an array of n integers, where the value of n is passed as an argument to the pre-filled function in your editor. This challenge uses a custom checker, so you can create any array of n integers.
+Note: Code stubs are provided for almost every language in which you must either fill in a blank (i.e., ____) or write your code in the area specified by comments.
 
+Method Signature
+Number Of Parameters: 1
+Parameters: [n]
+Returns: List or Vector
+
+Input Format
+A single integer, n.
+
+Constraints
+1 <= n <= 100
+The members returned by the list/vector/array must be integers.
+
+Output Format
+The function must return an array, list, or vector of n integers. Stub code in the editor prints this to stdout as a space, comma, or semicolon-separated list (depending on your submission language).
+Note: Your output need not match the Expected Output exactly; the size of your printed list is confirmed by a custom checker, which determines whether or not you passed each test case.
+")
+
+(
+    let [lines (line-seq (java.io.BufferedReader. *in*))] (
+        println(
+
+            (fn[n] (range n))
+
+            (Integer. (first lines))
+        )
+    )
+)
