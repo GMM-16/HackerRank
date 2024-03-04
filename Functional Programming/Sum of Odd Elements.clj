8780bfc394d1af8e797998ca8d6f2f8b5d789a10
GMM-16 <143779307+GMM-16@users.noreply.github.com>
2024-03-04 22:01:58 +0530

    Create Sum of Odd Elements.clj

--- Functional Programming/Sum of Odd Elements.clj
+++ Functional Programming/Sum of Odd Elements.clj
@@ -1,1 +1,9 @@
+comment("
+You are given a list. Return the sum of odd elements from the given list. The input and output portions will be handled automatically. You need to write a function with the recommended method signature.
 
+Constraints
+The list will have 1 -100 elements.
+Each element will be an integer X where -100 <= X <= 100 
+")
+
+(fn[lst] (reduce + (filter #(odd? %) lst)))
